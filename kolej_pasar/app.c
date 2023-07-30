80d358ce0907e82436b33912ad0260d9c84e4a83
Cranageist <cranageist@gmail.com>
2023-07-30 16:02:59 +0800

    initial commit

--- kolej_pasar/app.c
+++ kolej_pasar/app.c
@@ -2,6 +2,37 @@
 #include <stdio.h>
 #pragma warning(disable:4996)
 
-void main(void) {
+//function to repeat prompt until valid mode is selected
+void modeSelectionHandler(int* operationModePtr) {
+	while (*operationModePtr == 0) {
+		printf("> ");
+		rewind(stdin);
+		if (scanf("%d", operationModePtr) == NULL) continue;
 
+		switch (*operationModePtr) {
+		case 1:
+			printf("Administrator mode selected.\n");
+			return;
+
+		case 2:
+			printf("Student mode selected.\n");
+			return;
+
+		default:
+			*operationModePtr = 0;
+		}
+
+	}
+}
+
+void main(void) {
+	// int operationMode = 0 (repeat prompt) || 1 (admin) || 2 (student)
+	int operationMode = 0;
+	
+	printf("Kolej Pasar\n");
+	printf("==================================================\n");
+	printf("Type to select access mode\n1\t-\tadministrator mode\n2\t-\tstudent mode\n\n\n");
+	modeSelectionHandler(&operationMode);
+
+	system("pause");
 }
